270a26e8eeabdd32d3f581af8dbd20d08c298862
batuhan sahan <batuhansahan@trakya.edu.tr>
2017-10-30 15:09:00 +0000

    Logging the user with redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import { firebaseApp } from './firebase';
+import { logUser } from './actions';
+import reducer from './reducers';
 
+
+import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+
+const store = createStore(reducer);
 
 firebaseApp.auth().onAuthStateChanged(user => {
     if (user) {
         console.log('user has signed in or up', user);
-        browserHistory.push('app');
+        const { email } = user;
+        store.dispatch(logUser(email));
+        browserHistory.push('/app');
     } else {
         console.log('user has sign out or still need sign in');
         browserHistory.replace('/signin');
@@ -18,10 +27,12 @@ firebaseApp.auth().onAuthStateChanged(user => {
 })
 
 ReactDOM.render(
-        <Router path="/" history={browserHistory}>
-            <Route path="/app" component={App} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-        </Router>,
+        <Provider store={store}>
+            <Router path="/" history={browserHistory}>
+                <Route path="/app" component={App} />
+                <Route path="/signin" component={SignIn} />
+                <Route path="/signup" component={SignUp} />
+            </Router>
+        </Provider>,
         document.getElementById('root')
 )
