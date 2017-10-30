37463d51be4183cfde8d2ec5420fd531e2415396
batuhan sahan <batuhansahan@trakya.edu.tr>
2017-10-30 13:20:37 +0000

    Setting up sign up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class SignUp extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: {
+                message: ''
+            }
+        }
+    }
+    
+    signUp(){
+        console.log('this.state',this.state);
+        const { email, password } = this.state;
+        firebaseApp.auth().createUserWithEmailAndPassword(email,password)
+            .catch(error => {
+                this.setState({error })
+            })
+    }
+    
     render(){
         return(
-            <div>Sign Up</div>
+            <div className="form-inline" style={{margin: '5%'}}>
+                <h2>Sign Up</h2>
+                <div className="form-group">
+                    <input 
+                        className="form-control"
+                        type="text"
+                        placeholder="E-mail"
+                        style={{marginRight: '5px'}}
+                        onChange = {event => this.setState({email: event.target.value})}
+                    />
+                    <input
+                        className="form-control"
+                        type="password"
+                        placeholder="password"
+                        onChange={event => this.setState({password: event.target.value})}
+                    />
+                    <button
+                        className="btn btn-primary"
+                        type="button"
+                        onClick={() => this.signUp()}
+                    >
+                        Sign Up
+                    </button>
+                </div>
+                <div>{this.state.error.message}</div>
+            </div>
         )
     }
 }
