cfea21be5de5e81a22c2330d1dc420d95e7dddfa
batuhan sahan <batuhansahan@trakya.edu.tr>
2017-10-31 12:01:30 +0000

    Adding add goal function and using firebase database

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { firebaseApp } from '../firebase';
-
+import AddGoal from './AddGoal';
 
 class App extends Component {
     signOut(){
@@ -11,6 +11,9 @@ class App extends Component {
     render(){
         return (
             <div>
+                <h3>Goals</h3>
+                <AddGoal />
+                <div>Goal List</div>
                 <button 
                     className="btn btn-danger"
                     onClick={() => this.signOut()}
